ebca472ec39e84fdf2e9f3d4c880ea19ce22dde5
jiga972 <jiga972@gmail.com>
2020-01-24 11:47:18 +0100

    First page

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-import os
-import logging
+#import os
+#import logging
 
 #logging.warn(os.environ["DUMMY"])
 
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,10 +17,41 @@ ma = Marshmallow(app)
 from models import Product
 from schemas import products_schema
 
-@app.route('/products')
-def products():
-    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
-    return products_schema.jsonify(products)
+@app.route('/')
+def home():
+    products = db.session.query(Product).all()
+
+    return render_template('home.html', products=products)
+
+@app.route('/<int:id>')
+def product_html(id):
+    product = db.session.query(Product).get(id)
+    return render_template('product.html', product=product)
+
+#@app.route('/products', methods=["GET"])
+#def get_products():
+#   products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+#   return products_schema.jsonify(products)
+
+#@app.route('/products/<int:id>', methods=["GET"])
+#def get_product(id):
+#    product = db.session.query(Product).get(id) # SQLAlchemy request => 'SELECT * FROM products'
+ #   return product_schema.jsonify(product)
+
+#@app.route('/products', methods=["POST"])
+#def post_product():
+#    product = Product(name="Jimmy")
+#    db.session.add(product)
+#    db.session.commit() # SQLAlchemy request => 'SELECT * FROM products'
+#    return product_schema.jsonify(product)
+
+#@app.route('/products/<int:id>', methods=["DELETE"])
+#def delete_product(id):
+#    product = db.session.query(Product).get(id)
+#    db.session.delete(product)
+#    db.session.commit()
+#    return product_schema.jsonify(product)
+
 
 #@app.route('/hello')
 #def hello():
